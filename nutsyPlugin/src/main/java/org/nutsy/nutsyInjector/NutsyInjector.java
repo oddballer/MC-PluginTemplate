2709691e848ff040d15e656d3ea31e1bf0553a84
AJ <lapage3@gmail.com>
2024-07-01 16:43:57 -0600

    1.0 template

---
+++ nutsyPlugin/src/main/java/org/nutsy/nutsyInjector/NutsyInjector.java
@@ -0,0 +1,15 @@
+package org.nutsy.nutsyInjector;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class NutsyInjector extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+    // Fired when plugin is disabled
+    @Override
+    public void onDisable() {
+
+    }
+
+}
